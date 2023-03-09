cc1070d1b3d93ff41430bda9dde21fa976e4484e
Barath <barathdhilip713@gmail.com>
2023-03-09 17:22:57 +0530

    update

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -24,11 +24,21 @@ let result = document.getElementById("password__result")
 let generate = document.getElementsByClassName("btn")
 console.log(generate);
 let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let lowercase = "abcdefghijklmnopqrstuvwxyz"
 generate[0].addEventListener("click",e=>{
    
     let res =""
-    
-    if (upp_check.checked == true){
+
+    if (upp_check.checked == true && low_check.checked== true && num_check.checked==false && symbol_check.checked == false){
+        let  charlength = document.getElementById('length').value
+        let upp_low = uppercase + lowercase
+        for(let i =0;i<charlength;i++){
+            res+= upp_low.charAt(Math.random()*upp_low.length)
+        }
+        console.log(res);
+        result.value = res;
+    }
+    if (upp_check.checked == true && low_check.checked==false && num_check.checked==false && symbol_check.checked == false){
         let  charlength = document.getElementById('length').value
         for(let i =0;i<charlength;i++){
             res+= uppercase.charAt(Math.random()*uppercase.length)
@@ -37,6 +47,7 @@ generate[0].addEventListener("click",e=>{
         result.value = res;
 
     }
+
 })
 
 
