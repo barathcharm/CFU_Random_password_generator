064f9f5df435b76bb33648651f3d89887613a74a
Barath <barathdhilip713@gmail.com>
2023-03-09 17:42:07 +0530

    update

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -25,6 +25,8 @@ let generate = document.getElementsByClassName("btn")
 console.log(generate);
 let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 let lowercase = "abcdefghijklmnopqrstuvwxyz"
+let num ="1234567890"
+let sym ="!@#$%^&*()|~.,/';[]{}-=+_?><"
 generate[0].addEventListener("click",e=>{
    
     let res =""
@@ -47,6 +49,118 @@ generate[0].addEventListener("click",e=>{
         result.value = res;
 
     }
+
+    if (upp_check.checked == true && low_check.checked== true && num_check.checked==true && symbol_check.checked == false) {
+        let  charlength = document.getElementById('length').value
+        let upp_num_low =uppercase+lowercase+num
+        for(let i =0;i<charlength;i++){
+            res+= upp_num_low.charAt(Math.random()*upp_num_low.length)
+        }
+        console.log(res);
+        result.value = res; 
+    }
+
+    if (upp_check.checked == true && low_check.checked== true && num_check.checked==true && symbol_check.checked == true) {
+        let  charlength = document.getElementById('length').value
+        let upp_num_low_sym =uppercase+lowercase+num+sym
+        for(let i =0;i<charlength;i++){
+            res+= upp_num_low_sym.charAt(Math.random()*upp_num_low_sym.length)
+        }
+        console.log(res);
+        result.value = res; 
+    }
+
+    if (upp_check.checked == true && low_check.checked== true && num_check.checked==false && symbol_check.checked == false){
+        let  charlength = document.getElementById('length').value
+        let upp_low = uppercase + lowercase
+        for(let i =0;i<charlength;i++){
+            res+= upp_low.charAt(Math.random()*upp_low.length)
+        }
+        console.log(res);
+        result.value = res;
+    }
+
+    if (upp_check.checked == false && low_check.checked== true && num_check.checked==false && symbol_check.checked == false){
+        let  charlength = document.getElementById('length').value
+        
+        for(let i =0;i<charlength;i++){
+            res+= lowercase.charAt(Math.random()*lowercase.length)
+        }
+        console.log(res);
+        result.value = res;
+    }
+    
+    if (upp_check.checked == false && low_check.checked== false && num_check.checked==true && symbol_check.checked == false){
+        let  charlength = document.getElementById('length').value
+        
+        for(let i =0;i<charlength;i++){
+            res+= num.charAt(Math.random()*num.length)
+        }
+        console.log(res);
+        result.value = res;
+    }
+    
+    if (upp_check.checked == false && low_check.checked== false && num_check.checked==false && symbol_check.checked == true){
+        let  charlength = document.getElementById('length').value
+        
+        for(let i =0;i<charlength;i++){
+            res+= sym.charAt(Math.random()*sym.length)
+        }
+        console.log(res);
+        result.value = res;
+    }
+    
+    if (upp_check.checked == true && low_check.checked== false && num_check.checked==true && symbol_check.checked == false){
+        let  charlength = document.getElementById('length').value
+        let upp_num = uppercase + num
+        for(let i =0;i<charlength;i++){
+            res+= upp_num.charAt(Math.random()*upp_num.length)
+        }
+        console.log(res);
+        result.value = res;
+    }
+
+    if (upp_check.checked == true && low_check.checked== false && num_check.checked==false && symbol_check.checked == true){
+        let  charlength = document.getElementById('length').value
+        let upp_sym = uppercase + sym
+        for(let i =0;i<charlength;i++){
+            res+= upp_sym.charAt(Math.random()*upp_sym.length)
+        }
+        console.log(res);
+        result.value = res;
+    }
+    if (upp_check.checked == false && low_check.checked== true && num_check.checked==true && symbol_check.checked == false){
+        let  charlength = document.getElementById('length').value
+        let low_num = lowercase + num
+        for(let i =0;i<charlength;i++){
+            res+= low_num.charAt(Math.random()*low_num.length)
+        }
+        console.log(res);
+        result.value = res;
+    }
+    if (upp_check.checked == false && low_check.checked== true && num_check.checked==false && symbol_check.checked == true){
+        let  charlength = document.getElementById('length').value
+        let low_sym = lowercase + sym
+        for(let i =0;i<charlength;i++){
+            res+= low_sym.charAt(Math.random()*low_sym.length)
+        }
+        console.log(res);
+        result.value = res;
+    }
+    if (upp_check.checked == false && low_check.checked== false && num_check.checked==true && symbol_check.checked == true){
+        let  charlength = document.getElementById('length').value
+        let num_sym = num+sym
+        for(let i =0;i<charlength;i++){
+            res+= num_sym.charAt(Math.random()*num_sym.length)
+        }
+        console.log(res);
+        result.value = res;
+    }
+
+
+
+
+    
 
 })
 
